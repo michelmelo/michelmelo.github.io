e8ee13ba3edfab621fa8da7cc8e4a9b7a20f74d8
Your Name <you@example.com>
2020-03-04 16:24:44 +0000

    teste

---
+++ build/temp/development/MyApp/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\Student_04\Downloads\ext-6.2.0-gpl\michelmelo.github.io/build/.sass-cache'
---
+++ build/temp/development/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\Student_04\Downloads\ext-6.2.0-gpl\michelmelo.github.io\app.js
